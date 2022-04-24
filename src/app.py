555400e79e21f8c1690980583072dec87c5e9987
Vanderlei Junior <dev.vafj@gmail.com>
2022-04-24 19:39:11 -0300

    Fix uwsgi.ini

--- src/app.py
+++ src/app.py
@@ -26,4 +26,12 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
     app.run(port=5000)
