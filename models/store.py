36208d183ee975fcebf555e38b5388105e5ea9f4
Vanderlei Junior <dev.vafj@gmail.com>
2022-04-25 19:53:44 -0300

    Fix backref on StoreModel

--- models/store.py
+++ models/store.py
@@ -7,7 +7,7 @@ class StoreModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    items = db.relationship('ItemModel', lazy='dynamic')
+    items = db.relationship('ItemModel', lazy='dynamic', backref='store')
 
     def __init__(self, name) -> None:
         self.name = name
