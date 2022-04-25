2c10c839a18fd242f096aaa1ca06fd23e01cca59
Vanderlei Junior <dev.vafj@gmail.com>
2022-04-25 18:58:05 -0300

    Comment sqlite app config

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'vafj'
